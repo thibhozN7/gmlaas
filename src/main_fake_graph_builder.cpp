87165ded45502fdd9a2a2f298aa08f1f69677f2e
Thibaut Hozjan <thozjan@vgg>
2024-02-21 19:12:31 +0100

    bene

---
+++ src/main_fake_graph_builder.cpp
@@ -0,0 +1,17 @@
+#include <fake_graph_builder.hpp>
+#include <ros/master.h>
+#include <unistd.h>
+
+
+int main(int argc, char **argv){
+
+  ros::init(argc, argv, "main_fake_graph_builder");
+  ros::NodeHandle fake_graph_builder_node;
+  
+  FakeGraphBuilder fake_graph_builder(fake_graph_builder_node);
+  fake_graph_builder.createFakeGraph();
+
+  ros::spin();
+
+  return 0;
+}
