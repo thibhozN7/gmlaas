1b5542d8deab855d10812730f055374d1a4e8e82
matthieu <matthieu.marchal@etu.toulouse-inp.fr>
2024-02-28 17:05:01 +0000

    h

--- python_scripts/pre_h_computation.py
+++ python_scripts/pre_h_computation.py
@@ -35,36 +35,34 @@ class PreHMatrix:
         self.m_desired_dict = {}
         self.m_h_current_coordinates = np.array([])
         self.m_h_desired_coordinates = np.array([])
-
-    def storeCurrentGraphData(self):
-        # Store the timestamp, adjacency matrix, and indexed matrix from the graph data
-        self.adjacency_matrix = self.m_graph_sub.adjacency_matrix
-        self.current_indexed_matrix = self.m_graph_sub.indexed_matrix
-        self.isomorphism_list = self.m_graph_sub.indexed_matrix
-        rospy.loginfo("Current graph data stored.")
-
+    
     def buildReferenceIndexMatrix(self):
         # Read the reference index matrix from the reference tags dataset
-        with open(f"{package_dir}/datasets/referenc/tags_dataset.csv", "r") as file:
+        with open(f"{package_dir}/datasets/snapshots/reference_graph_dataset.csv", "r") as file:
             reader = csv.reader(file)
             next(reader)  # Skip the header row
             for row in reader:
                 self.m_reference_index_matrix = row[4]
-        rospy.loginfo("Reference index matrix built.")
+        rospy.loginfo("Reference index matrix obtained.")
+
+    def reshapeCurrentGraphData(self, msg):
+        # Store the timestamp, adjacency matrix, and indexed matrix from the graph data
+        self.current_indexed_matrix = msg.indexed_matrix
+        self.current_tags_id = msg.tags_id 
+        self.isomorphism_list =msg.isomorphism_list
+        self.current_coordinates = np.reshape(msg.coordinate_matrix,(len(self.current_tags_id),3))
+        rospy.loginfo("Current graph data reshaped.")
     
     def buildCurrentTagDict(self):
         # Store the current tag coordinates from the tag detections
-        for detection in self.m_graph_sub.detections:
-            tag_id = detection.id[0]
-            x = detection.pose.pose.pose.position.x
-            y = detection.pose.pose.pose.position.y
-            z = detection.pose.pose.pose.position.z
-            self.m_current_dict[tag_id] = [x, y, z]
+        for i in self.current_tags_id:
+            tag_id = self.current_tags_id[i]
+            self.m_current_dict[tag_id] = self.current_coordinates[i]
         rospy.loginfo("Current tag dictionary built.")
     
     def buildDesiredTagDict(self):
         # Build dictionaries for desired tag coordinates
-        with open(f"{package_dir}/datasets/desired/tags_dataset.csv", "r") as file:
+        with open(f"{package_dir}/datasets/snapshots/desired_frame_tags_dataset.csv", "r") as file:
             reader = csv.reader(file)
             next(reader)  # Skip the header row
             for row in reader:
@@ -76,14 +74,15 @@ class PreHMatrix:
         rospy.loginfo("Desired tag dictionary built.")
 
     def calculateCoordinates(self):
+        rospy.loginfo("Building the current and desired coordinates matrices...")
         # Calculate the current and desired coordinates for the isomorphism list
         for i in range(len(self.isomorphism_list) - 1):
-            ref_id = self.m_reference_index_matrix[self.isomorphism_list[i]]
-            if ref_id in self.m_desired_dict.keys():
-                self.m_h_desired_coordinates = np.append(self.m_h_desired_coordinates, self.m_desired_dict[ref_id])
-                cur_id = self.current_indexed_matrix[i]
-                self.m_h_current_coordinates = np.append(self.m_h_current_coordinates, self.m_current_dict[cur_id])
-        check = len(self.m_h_current_coordinates) == len(self.m_h_desired_coordinates)
+            ref_id = self.m_reference_index_matrix[self.isomorphism_list[i]] # Get the reference tag ID
+            if ref_id in self.m_desired_dict.keys(): # Check if the reference tag ID is in the desired dictionary
+                self.m_h_desired_coordinates = np.append(self.m_h_desired_coordinates, self.m_desired_dict[ref_id]) # Append the desired coordinates
+                cur_id = self.current_indexed_matrix[i] # Get the current tag ID
+                self.m_h_current_coordinates = np.append(self.m_h_current_coordinates, self.m_current_dict[cur_id]) # Append the current coordinates
+        check = len(self.m_h_current_coordinates) == len(self.m_h_desired_coordinates) # Check if the current and desired coordinates matrices match
         if check:
             rospy.loginfo("The current and desired coordinates matrices match.")
         else:
@@ -91,18 +90,19 @@ class PreHMatrix:
     
     def publishMatrix(self, current_coordinates, desired_coordinates):
         # Publish the current and desired coordinates as a PreHMsg message
+        rospy.loginfo("Publishing the current and desired coordinates matrices...")
         msg = PreHMsg()
         msg.current_coordinates = list(current_coordinates.flatten())
         msg.desired_coordinates = list(desired_coordinates.flatten())
         self.m_pre_h_pub.publish(msg)
         rospy.loginfo("Coordinates matrices published.")
 
-    def callback(self, m_isomorphism_list_sub, m_graph_sub, m_tag_sub):
+    def callback(self, msg):
         # Callback function for synchronized data
-        self.storeCurrentGraphData(m_graph_sub)
-        self.storeCurrentTagData(m_tag_sub)
         self.buildReferenceIndexMatrix()
-        self.buildDesiredDict()
+        self.reshapeCurrentGraphData(msg)
+        self.buildCurrentTagDict()
+        self.buildDesiredTagDict()
         self.calculateCoordinates()
         self.publishMatrix(self.m_h_current_coordinates, self.m_h_desired_coordinates)
 
